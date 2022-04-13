187b93a0ad1581d76d91b5f2e3e320971653f0e1
Logan Falzarano <lfalzar1@jhu.edu>
2022-04-13 16:10:40 -0400

    finishing bmi-app

--- server.js
+++ server.js
@@ -1,13 +1,14 @@
-const http = require("http");
-const fs = require("fs");
-const port = process.env.PORT || 7000;
-const file = fs.readFileSync("./index.html", "utf8");
+const path = require("path");
+const express = require("express"); //returns a function which we capture in the express variable
+const app = express(); //set app equal to express
+const port = process.env.PORT || 7000; //
 
-const server = http.createServer((request, response) => {
-  response.writeHead(200, { "Content-Type": "text/html" });
-  response.write(file);
-  response.end();
+//handles HTTP get request (first arg is a path)
+app.get("/", (req, res) => { //req = incoming, res = outgoing
+  res.sendFile(path.join(__dirname, "index.html")); //send file transfers a file at the given path sets header file
+  //__dirname returns path of JS folder
 });
 
-server.listen(port);
-console.log(`Listening! (port ${port})`);
+app.listen(port, () => { //binds and listens for connectiosn on the specified port
+  console.log(`Express app listening at http://localhost:${port}`);
+});
